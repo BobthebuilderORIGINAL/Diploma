89163b497c246a8433864bcc6e7971c6604f3d4a
BobthebuilderORIGINAL <b.hadjiyankov2021@pmg-pernik.com>
2026-01-15 12:53:54 +0200

    part 1

---
+++ CarSimulatorWebShop/WebShop.Infrastruture/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using WebShop.Infrastructure.Data.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebShop.Infrastruture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
